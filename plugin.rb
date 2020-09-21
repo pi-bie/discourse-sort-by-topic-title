df86b53d26e3dacaa8e00d29255682449e8e5c54
pi-bie <60271026+pi-bie@users.noreply.github.com>
2020-09-21 23:15:39 +0200

    now also exclude pinned topics from ordering by title

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-sort-by-topic-title
 # about: A simple plugin to allow sorting topics by title
-# version: 0.1
+# version: 0.2
 # authors: piBie
 # url: https://github.com/pi-bie/discourse-sort-by-topic-title
 
@@ -13,6 +13,19 @@ after_initialize do
 		class ::TopicQuery
 			SORTABLE_MAPPING["title"] = "title"
 
+			module SortByTitleCreateList
+				def create_list(filter, options = {}, topics = nil)
+					topics ||= default_results(options)
+					topics = yield(topics) if block_given?
+					
+					alloptions = options.merge(@options)
+					if alloptions[:order] == 'title'
+						topics = prioritize_pinned_topics(topics, alloptions)
+					end
+					super(filter, options, topics)
+				end
+			end
+			prepend SortByTitleCreateList
 		end
 	
 	end
